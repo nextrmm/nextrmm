70aed8dba838f4709a1f70cea11edc5442644f24
Albert Chen <albertchen.dev@outlook.com>
2023-12-11 12:04:10 +0800

    fix: delete unused export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,4 +6,4 @@ import { authOptions } from "~/server/auth";
 const handler = async function auth(req: NextApiRequest, res: NextApiResponse) {
   return NextAuth(req, res, authOptions());
 };
-export { handler as GET, handler as POST };
+export { handler as POST };
