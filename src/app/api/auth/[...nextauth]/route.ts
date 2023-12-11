789f7d72cdef46d5dd1eb2c591b4f3c1ecaa7e24
Albert Chen <albertchen.dev@outlook.com>
2023-12-11 12:12:21 +0800

    fix: fit auth function in next.js app route pattern.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextRequest } from "next/server";
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-// const handler = NextAuth(authOptions);
-const handler = async function auth(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, authOptions());
-};
-export { handler as POST };
+interface RouteHandlerContext {
+  params: { nextauth: string[] };
+}
+
+async function auth(req: NextRequest, context: RouteHandlerContext) {
+  // ...some logic
+  return await NextAuth(req, context, authOptions());
+}
+
+export { auth as GET, auth as POST };
